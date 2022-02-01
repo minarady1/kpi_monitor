98346d3ead08224a5b457499daabda47be7379cf
minarady1 <minarady@gmail.com>
2022-02-01 17:02:47 +0100

    removing useless files

--- cdf3d.sh
+++
@@ -1,6 +0,0 @@
-python kpi_3dplot.py 'numNeighbors' 'Number of neighbors' &
-python kpi_3dplot.py 'maxBufferSize' 'Packet buffer occupancy (packets)' &
-python kpi_3dplot.py 'latency' 'Latency (secs)' &
-python kpi_3dplot.py 'dutyCycle' 'Radio duty cycle %' &
-python kpi_3dplot.py 'dagRank' 'DAG Rank'
-$SHELL
--- kpi_live_parser.py
+++
@@ -1,9 +0,0 @@
-'''
-KPI processing and analytics toolkit for opentestbed
-Author: Mina Rady <mina1.rady@orange.com>, July 2020
-'''
-import time
-
-while(True):
-	execfile('kpi_stats_compo_parser.py')
-	time.sleep(10)
